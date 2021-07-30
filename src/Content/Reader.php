72528d501cb6d12ea09a3960037ca17f3b5c58ef
Enjoys <deadly.info@gmail.com>
2021-07-30 12:23:53 +0300

    added property `path` for simple validate on type

--- src/Content/Reader.php
+++ src/Content/Reader.php
@@ -26,6 +26,10 @@ class Reader
      * @var false|string
      */
     private $content;
+    /**
+     * @var false|string
+     */
+    private $path;
 
     /**
      * @var array{css: array, js: array}
@@ -53,7 +57,7 @@ class Reader
         $this->environment = $environment;
         $this->asset = $asset;
         $this->logger = $logger ?? new NullLogger();
-
+        $this->path = $this->asset->getPath();
         $this->content = $this->getContent();
         $this->minifyOptions = $minifyOptions;
     }
@@ -63,14 +67,12 @@ class Reader
      */
     public function getContents(): string
     {
-        $path = $this->asset->getPath();
-
-        if ($this->content === false || $path === false) {
-            $this->logger->notice(sprintf('Nothing return: path is `%s`', (string)$path));
+        if ($this->content === false || $this->path === false) {
+            $this->logger->notice(sprintf('Nothing return: path is `%s`', $this->asset->getOrigPath()));
             return '';
         }
 
-        $replaceRelativeUrls = new ReplaceRelative($this->content, $path, $this->asset, $this->environment);
+        $replaceRelativeUrls = new ReplaceRelative($this->content, $this->path, $this->asset, $this->environment);
         $replaceRelativeUrls->setLogger($this->logger);
         $this->content = $replaceRelativeUrls->getContent();
 
@@ -81,7 +83,7 @@ class Reader
                 $this->asset->getType(),
                 $this->minifyOptions
             )->getContent() . "\n";
-            $this->logger->info(sprintf('Minify: %s', $path));
+            $this->logger->info(sprintf('Minify: %s', $this->path));
         }
         return $this->content;
     }
@@ -92,11 +94,11 @@ class Reader
      */
     private function getContent()
     {
-        if (false !== $path = $this->asset->getPath()) {
+        if ($this->path !== false) {
             if ($this->asset->isUrl()) {
-                return $this->readUrl($path);
+                return $this->readUrl($this->path);
             }
-            return $this->readFile($path);
+            return $this->readFile($this->path);
         }
         return false;
     }

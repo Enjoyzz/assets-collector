67e1150edd495636188a40476f82746acd6d041a
enjoys <deadly.info@gmail.com>
2023-02-09 18:42:43 +0300

    refactor \Enjoys\AssetsCollector\Helpers::getHttpScheme

--- src/Helpers.php
+++ src/Helpers.php
@@ -9,24 +9,23 @@ use function Enjoys\FileSystem\createDirectory;
 use function Enjoys\FileSystem\CreateSymlink;
 use function Enjoys\FileSystem\writeFile;
 
-class Helpers
+final class Helpers
 {
 
     public static function getHttpScheme(): string
     {
-        $scheme = 'http';
         if (isset($_SERVER['HTTP_SCHEME'])) {
-            return (string)$_SERVER['HTTP_SCHEME'];
+            return $_SERVER['HTTP_SCHEME'];
         }
 
-        if (isset($_SERVER['HTTPS']) && \strtolower((string)$_SERVER['HTTPS']) != 'off') {
+        if (isset($_SERVER['HTTPS']) && \strtolower($_SERVER['HTTPS']) != 'off') {
             return 'https';
         }
 
         if (isset($_SERVER['SERVER_PORT']) && 443 == (int)$_SERVER['SERVER_PORT']) {
             return 'https';
         }
-        return $scheme;
+        return 'http';
     }
 
     /**
